a9b10000366303d3be3f7a9db3d133076337a267
Niklas Halonen <niklas.halonen@protonmail.com>
2025-06-20 20:46:08 +0300

    leanblueprint: add checks
    Added some checks to ensure expected files are created by the web and pdf subcommand of the tool.
    The tests also highlight that a LaTeX distribution and `latexmk` is required for proper functionality of the tool.

--- pkgs/development/python-modules/leanblueprint/default.nix
+++ pkgs/development/python-modules/leanblueprint/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   buildPythonPackage,
   fetchFromGitHub,
 
@@ -17,7 +18,7 @@
   jinja2,
   gitpython,
 }:
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "leanblueprint";
   version = "0.0.10";
   pyproject = true;
@@ -25,7 +26,7 @@ buildPythonPackage {
   src = fetchFromGitHub {
     repo = "leanblueprint";
     owner = "PatrickMassot";
-    rev = "v0.0.10";
+    tag = "v${version}";
     hash = "sha256-CUYdxEXgTf2vKDiOoeW4RV6tQ6prFhA4qMc0olZtZBM=";
   };
 
@@ -43,7 +44,43 @@ buildPythonPackage {
     gitpython
   ];
 
+  nativeCheckInputs = [
+    pkgs.git
+    pkgs.texlive.combined.scheme-small
+    pkgs.texlivePackages.latexmk
+  ];
+
   pythonImportsCheck = [ "leanblueprint" ];
+
+  checkPhase = ''
+    runHook preCheck
+
+    # A git repository is required by leanblueprint
+    git init
+
+    # A lakefile is required by leanblueprint
+    echo > lakefile.lean
+
+    # Copy minimal blueprint
+    mkdir -p blueprint/src
+    cp -r ${./test/blueprint/src}/. blueprint/src
+
+    # Check web HTML output
+    $out/bin/leanblueprint web
+    grep -q "Leanblueprint Test" blueprint/web/index.html
+    grep -q "Test Chapter" blueprint/web/index.html
+    grep -q "Test Chapter" blueprint/web/sect0001.html
+    grep -q "def:one_add_one_eq_two" blueprint/web/sect0001.html
+    grep -q "sect0001.html#def:one_add_one_eq_two" blueprint/web/dep_graph_document.html
+    test -d blueprint/web/js
+    test -d blueprint/web/styles
+
+    # Check print PDF output
+    $out/bin/leanblueprint pdf
+    test -f blueprint/print/print.pdf
+
+    runHook postCheck
+  '';
 
   meta = {
     description = "This plasTeX plugin allowing to write blueprints for Lean 4 projects";
